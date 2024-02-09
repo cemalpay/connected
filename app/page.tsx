45e2653ef1c2ced11a1a74e5abe31f887eac64d3
cemalpay <cem.alpay.tas@common.studio>
2024-02-09 17:57:42 +0300

    install supabase nextjs

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Notes from "@/app/notes/pages";
+
+export default function page() {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notes />
+    </div>
+  );
+}
